64a4609b99ea34343eef179dd35727dc03b76948
healthpackdev <yasinkadirkilic@gmail.com>
2021-07-15 18:48:06 +0300

    🚑 use preact instead of react to reduce bundle size

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const withImages = require("next-images");
 
 module.exports = withImages({
 	reactStrictMode: true,
+	webpack(config) {
+		// replace react with preact to reduce bundle size and faster DevServer
+		Object.assign(config.resolve.alias, {
+			"react-dom": "preact/compat",
+			react: "preact/compat",
+		});
+	},
 	async redirects() {
 		return [
 			{
