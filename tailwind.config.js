9c3332d834b28acfcc6ea0cb773c4852b0f4180d
Barış DEMİRCİ <demirci.baris38@gmail.com>
2021-12-17 18:53:25 +0000

    🔒️ Update tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 module.exports = {
 	mode: "jit",
-	purge: [
+	content: [
 		"./pages/**/*.{js,ts,jsx,tsx}",
 		"./components/**/*.{js,ts,jsx,tsx}",
 		"./libs/**/*.{js,ts,jsx,tsx}",
