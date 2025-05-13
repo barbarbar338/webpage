b7d1254c3632d44414190dab9eff55427fcf1c91
Barış DEMİRCİ <hi@338.rocks>
2025-05-13 23:41:37 +0300

    copy cv to public

---
+++ copy-cv.js
@@ -0,0 +1,15 @@
+import fs from "fs";
+import path from "path";
+
+const cv_file = path.join(process.cwd(), "cv", "main.pdf");
+const public_path = path.join(process.cwd(), "public", "cv.pdf");
+
+async function main() {
+    try {
+        await fs.promises.copyFile(cv_file, public_path);
+        console.log("CV copied successfully");
+    } catch (error) {
+        console.error("Error copying CV:", error);
+    }
+}
+main();
