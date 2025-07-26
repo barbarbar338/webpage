048e71d75d8e0ccd1ada35207b4e75ad541441ed
Barış DEMİRCİ <hi@338.rocks>
2025-07-26 19:33:06 +0300

    Upgrade deps

--- copy-cv.js
+++ copy-cv.js
@@ -5,11 +5,11 @@ const cv_file = path.join(process.cwd(), "cv", "main.pdf");
 const public_path = path.join(process.cwd(), "public", "cv.pdf");
 
 async function main() {
-    try {
-        await fs.promises.copyFile(cv_file, public_path);
-        console.log("CV copied successfully");
-    } catch (error) {
-        console.error("Error copying CV:", error);
-    }
+	try {
+		await fs.promises.copyFile(cv_file, public_path);
+		console.log("CV copied successfully");
+	} catch (error) {
+		console.error("Error copying CV:", error);
+	}
 }
 main();
